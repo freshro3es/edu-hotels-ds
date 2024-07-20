5b0fb2b1c50e0814e761881183281e09ab915f38
freshro3es <igorkarpy@gmail.com>
2024-07-20 14:11:25 +0300

    Added dotenv and updated database file path

--- 3-express-rest-api/database.js
+++ 3-express-rest-api/database.js
@@ -1,7 +1,11 @@
 import sqlite3 from 'sqlite3';
+import dotenv from 'dotenv';
+
+dotenv.config();
 sqlite3.verbose();
 
-const db = new sqlite3.Database('users.db');
+const dbFilePath = process.env.DB_FILE || 'default.db';
+const db = new sqlite3.Database(dbFilePath);
 
 db.serialize(() => {
     db.run(`CREATE TABLE IF NOT EXISTS users (
